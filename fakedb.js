65a7ebbd14a49825ad358fe4eb8bfd639641b711
Ian Guelman <ianguelman@gmail.com>
2019-06-10 22:18:52 -0300

    js

---
+++ fakedb.js
@@ -0,0 +1,12 @@
+var db = {
+    data: [{
+        id: 1,
+        data: "1/1/2019",
+        nome: "a",
+        titulo: "b",
+        corpo: "c",
+        imagem: "d",
+        comentarios : [],
+        likes : 0
+    }]
+}
