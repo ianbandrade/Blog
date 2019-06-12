146f3b6b0cc8deaf06480eb4bb0773390aec37f8
Ian Guelman <ianguelman@gmail.com>
2019-06-11 22:20:06 -0300

    comments

--- fakedb.js
+++ fakedb.js
@@ -6,7 +6,7 @@ var dbfake = {
         titulo: "Titulo 1",
         corpo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam condimentum diam diam, non varius eros vehicula vel. Maecenas faucibus dignissim libero hendrerit semper. Vestibulum tempus, quam eget finibus pulvinar, sem ex vulputate risus, ut semper nisl augue ac sapien. Donec bibendum eros odio, posuere volutpat nisl ullamcorper at. Vivamus ac feugiat mauris. Nunc nec egestas lorem. Proin lacinia interdum luctus. Maecenas odio turpis, ultricies a nisl in, scelerisque auctor est. Donec ut scelerisque mi, id volutpat risus. Etiam id sem lacinia, scelerisque dolor eget, laoreet arcu. Curabitur nisl ex, aliquet sit amet sodales sit amet, accumsan semper nibh. Nullam laoreet semper volutpat. Maecenas suscipit elit nisl, nec semper augue dignissim quis. Aenean finibus massa ligula, vel gravida odio tincidunt tempus. Etiam luctus imperdiet est, non blandit elit vestibulum sed.",
         imagem: "http://lorempixel.com/800/300/food/1",
-        comentarios : [],
+        comentarios : [{autor: "autor1",comentario:"Teste1", data:"1/1/2019"}, {autor: "autor1",comentario:"Teste1", data:"1/1/20192"}],
         likes : 0
     },
     {
@@ -16,7 +16,7 @@ var dbfake = {
         titulo: "Titulo 2",
         corpo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam condimentum diam diam, non varius eros vehicula vel. Maecenas faucibus dignissim libero hendrerit semper. Vestibulum tempus, quam eget finibus pulvinar, sem ex vulputate risus, ut semper nisl augue ac sapien. Donec bibendum eros odio, posuere volutpat nisl ullamcorper at. Vivamus ac feugiat mauris. Nunc nec egestas lorem. Proin lacinia interdum luctus. Maecenas odio turpis, ultricies a nisl in, scelerisque auctor est. Donec ut scelerisque mi, id volutpat risus. Etiam id sem lacinia, scelerisque dolor eget, laoreet arcu. Curabitur nisl ex, aliquet sit amet sodales sit amet, accumsan semper nibh. Nullam laoreet semper volutpat. Maecenas suscipit elit nisl, nec semper augue dignissim quis. Aenean finibus massa ligula, vel gravida odio tincidunt tempus. Etiam luctus imperdiet est, non blandit elit vestibulum sed.",
         imagem: "http://lorempixel.com/800/300/food/2",
-        comentarios : [],
+        comentarios : [{autor: "autor2",comentario:"Teste2", data:"2/1/2019"}],
         likes : 0
     },{
         id: 3,
@@ -25,7 +25,7 @@ var dbfake = {
         titulo: "Titulo 3",
         corpo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam condimentum diam diam, non varius eros vehicula vel. Maecenas faucibus dignissim libero hendrerit semper. Vestibulum tempus, quam eget finibus pulvinar, sem ex vulputate risus, ut semper nisl augue ac sapien. Donec bibendum eros odio, posuere volutpat nisl ullamcorper at. Vivamus ac feugiat mauris. Nunc nec egestas lorem. Proin lacinia interdum luctus. Maecenas odio turpis, ultricies a nisl in, scelerisque auctor est. Donec ut scelerisque mi, id volutpat risus. Etiam id sem lacinia, scelerisque dolor eget, laoreet arcu. Curabitur nisl ex, aliquet sit amet sodales sit amet, accumsan semper nibh. Nullam laoreet semper volutpat. Maecenas suscipit elit nisl, nec semper augue dignissim quis. Aenean finibus massa ligula, vel gravida odio tincidunt tempus. Etiam luctus imperdiet est, non blandit elit vestibulum sed.",
         imagem: "http://lorempixel.com/800/300/food/3",
-        comentarios : [],
+        comentarios : [{autor: "autor3",comentario:"Teste3", data:"3/1/2019"}],
         likes : 0
     },{
         id: 4,
@@ -34,7 +34,7 @@ var dbfake = {
         titulo: "Titulo 4",
         corpo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam condimentum diam diam, non varius eros vehicula vel. Maecenas faucibus dignissim libero hendrerit semper. Vestibulum tempus, quam eget finibus pulvinar, sem ex vulputate risus, ut semper nisl augue ac sapien. Donec bibendum eros odio, posuere volutpat nisl ullamcorper at. Vivamus ac feugiat mauris. Nunc nec egestas lorem. Proin lacinia interdum luctus. Maecenas odio turpis, ultricies a nisl in, scelerisque auctor est. Donec ut scelerisque mi, id volutpat risus. Etiam id sem lacinia, scelerisque dolor eget, laoreet arcu. Curabitur nisl ex, aliquet sit amet sodales sit amet, accumsan semper nibh. Nullam laoreet semper volutpat. Maecenas suscipit elit nisl, nec semper augue dignissim quis. Aenean finibus massa ligula, vel gravida odio tincidunt tempus. Etiam luctus imperdiet est, non blandit elit vestibulum sed.",
         imagem: "http://lorempixel.com/800/300/food/4",
-        comentarios : [],
+        comentarios : [{autor: "autor4",comentario:"Teste4", data:"4/1/2019"}],
         likes : 0
     },{
         id: 5,
@@ -43,7 +43,7 @@ var dbfake = {
         titulo: "Titulo 5",
         corpo: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam condimentum diam diam, non varius eros vehicula vel. Maecenas faucibus dignissim libero hendrerit semper. Vestibulum tempus, quam eget finibus pulvinar, sem ex vulputate risus, ut semper nisl augue ac sapien. Donec bibendum eros odio, posuere volutpat nisl ullamcorper at. Vivamus ac feugiat mauris. Nunc nec egestas lorem. Proin lacinia interdum luctus. Maecenas odio turpis, ultricies a nisl in, scelerisque auctor est. Donec ut scelerisque mi, id volutpat risus. Etiam id sem lacinia, scelerisque dolor eget, laoreet arcu. Curabitur nisl ex, aliquet sit amet sodales sit amet, accumsan semper nibh. Nullam laoreet semper volutpat. Maecenas suscipit elit nisl, nec semper augue dignissim quis. Aenean finibus massa ligula, vel gravida odio tincidunt tempus. Etiam luctus imperdiet est, non blandit elit vestibulum sed.",
         imagem: "http://lorempixel.com/800/300/food/5",
-        comentarios : [],
+        comentarios : [{autor: "autor5",comentario:"Teste5", data:"5/1/2019"}],
         likes : 0
     }]
 }
